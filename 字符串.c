0a0170a429a952dabab2fca0bb55f31792fba623
Hanwng <110345235+Hanwng@users.noreply.github.com>
2022-08-06 17:12:24 +0800

    Add files via upload

---
+++ 字符串.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	char arr1[] = "abc"; 
+	char arr2[] = { 'a','b','c' };
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", strlen(arr2));
+	 
+	return 0;
+
+
+
+
+
+}
