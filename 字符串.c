1b121fa3930522da3782eaec0d5a2ff0382cbc73
hanwang <110345235+Hanwng@users.noreply.github.com>
2022-08-09 21:49:48 +0800

    Add files via upload

--- 字符串.c
+++ 字符串.c
@@ -4,11 +4,12 @@
 
 int main()
 {
-	char arr1[] = "abc"; 
+	printf("c:\\test\32\\test.c");
+	/*char arr1[] = "abc"; 
 	char arr2[] = { 'a','b','c' };
 	printf("%d\n", strlen(arr1));
 	printf("%d\n", strlen(arr2));
-	 
+	 */
 	return 0;
 
 
