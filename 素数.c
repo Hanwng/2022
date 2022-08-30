1dfa6a964848dc3207a4f668e4f069317c0f500c
hanwang <110345235+Hanwng@users.noreply.github.com>
2022-08-30 08:54:05 +0800

    Add files via upload

---
+++ 素数.c
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<math.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (i ==j)
+//		{
+//			printf("%d ", i);
+//			count++;
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}  »ù´¡°æ
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int count = 0;
+//	for (i = 100; i < 200; i++)
+//	{
+//		for (j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//			if (j > sqrt(i))
+//			{
+//				printf("%d ", i);
+//				count++;
+//			}
+//	}
+//	printf("\ncount=%d", count);
+//	return 0;
+//}    ½ø½×°æ
+
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	int j = 0;
+//	for (i = 100; i <= 200; i += 2)
+//	{
+//		for (j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//			if (j > sqrt(i))
+//			{
+//				printf("%d ", i);
+//				count++;
+//			}
+//	}
+//	printf("\ncount=%d", count);
+//		return 0;
+//}  ÖÁ×ð°æ
