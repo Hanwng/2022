6a8bd84981d1a4b51395e926cab6d4e626f96557
shixiaobang <xiaobang.shi@outlook.com>
2022-09-10 13:39:22 +0800

    null

---
+++ factorial.c
@@ -0,0 +1,21 @@
+// #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int factorial(int n);
+
+int main(void)
+{
+	int n;
+
+	printf("Please input a integer: ");
+	scanf("%d", &n);
+
+	printf("%d! = %d\n", n, factorial(n));
+
+	return 0;
+}
+
+int factorial(int n)
+{
+	/* code */
+}
