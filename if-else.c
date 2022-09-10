69e691c25acaaeacfb75acb79fbe0ef6bdb7239a
shixiaobang <xiaobang.shi@outlook.com>
2022-09-10 14:47:38 +0800

    update if-else.c

---
+++ if-else.c
@@ -0,0 +1,19 @@
+// #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+
+	if (a < b)
+	{
+		printf("a < b\n");
+	}
+	else
+	{
+		printf("a >=b\n");
+	}
+
+	return 0;
+}
